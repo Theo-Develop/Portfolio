804f5b1e39c939c729d24ed3aa57fb9e5c0eb7ea
Théo Dressy <t.dressy@gmail.com>
2024-03-16 16:51:58 +0100

    settings Welcome section

--- src/sections/welcome/Welcome.jsx
+++ src/sections/welcome/Welcome.jsx
@@ -1,12 +1,14 @@
+import React from "react";
+
 function Welcome() {
     return (
         <section id="acceuil" className="welcome">
-            <h2>Acceuil</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ornare lectus sit amet est. Risus pretium quam vulputate dignissim suspendisse in est ante in. Convallis convallis tellus id interdum velit laoreet. Euismod elementum nisi quis eleifend quam adipiscing vitae. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Molestie nunc non blandit massa. Risus nec feugiat in fermentum. Placerat duis ultricies lacus sed turpis tincidunt id aliquet. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. At urna condimentum mattis pellentesque. Massa enim nec dui nunc. In fermentum posuere urna nec tincidunt praesent. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Vitae semper quis lectus nulla.
-            </p>
+            <div className="content">
+                <h1>Acceuil</h1>
+                <h1>Dévelopeur Web</h1>
+            </div>
         </section>
-    )
+    );
 }
 
 export default Welcome;
