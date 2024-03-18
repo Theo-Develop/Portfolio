25f88a04edc023a2bf06cd7e06871cd15f916c48
Théo Dressy <t.dressy@gmail.com>
2024-03-18 11:11:26 +0100

    add footer and settings css

--- src/SectionsRoutes.jsx
+++ src/SectionsRoutes.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Welcome from "./sections/welcome/Welcome";
 import Presentation from "./sections/presentation/Presentation";
 import Realization from "./sections/realization/Realization";
 import Skills from "./sections/skills/Skills";
 import Contact from "./sections/contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 
 // Set all Sections routes
@@ -18,6 +19,7 @@ function SectionsRoutes() {
             <Realization />
             <Skills />
             <Contact />
+            <Footer />
         </BrowserRouter>
     )
 }
