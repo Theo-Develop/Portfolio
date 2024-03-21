b371eef04345156fd08e01d4025202cca2e6f96a
Théo Dressy <t.dressy@gmail.com>
2024-03-21 17:01:44 +0100

    settings skills

---
+++ src/assets/data/skills.js
@@ -0,0 +1,95 @@
+import html from "../icons/skills/icons8-html.svg";
+import css from "../icons/skills/icons8-css3.svg";
+import javascript from "../icons/skills/icons8-javascript.svg";
+import react from "../icons/skills/icons8-react-native.svg";
+import sass from "../icons/skills/icons8-sass.svg";
+import node from "../icons/skills/icons8-nodejs.svg";
+import express from "../icons/skills/icons8-express-js.svg";
+import mongo from "../icons/skills/icons8-mongodb.svg";
+import mongoose from "../icons/skills/icons8-mongoose-logo.svg";
+import vscode from "../icons/skills/icons8-visual-studio-code.svg";
+import git from "../icons/skills/icons8-git.svg";
+import github from "../icons/skills/icons8-github.svg";
+import trello from "../icons/skills/icons8-trello.svg";
+import postman from "../icons/skills/icons8-postman-is-the-only-complete-api-development-environment-70.png";
+import vercel from "../icons/skills/logo-vercel.svg";
+
+const skillsData = [
+    {
+        name: "HTML",
+        icon: html,
+        category: "Frontend",
+    },
+    {
+        name: "CSS",
+        icon: css,
+        category: "Frontend",
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+        category: "Frontend",
+    },
+    {
+        name: "React",
+        icon: react,
+        category: "Frontend",
+    },
+    {
+        name: "Sass",
+        icon: sass,
+        category: "Frontend",
+    },
+    {
+        name: "Node.js",
+        icon: node,
+        category: "Backend",
+    },
+    {
+        name: "Express.js",
+        icon: express,
+        category: "Backend",
+    },
+    {
+        name: "MongoDB",
+        icon: mongo,
+        category: "Backend",
+    },
+    {
+        name: "Mongoose",
+        icon: mongoose,
+        category: "Backend",
+    },
+    {
+        name: "Visual Studio Code",
+        icon: vscode,
+        category: "Mes outils",
+    },
+    {
+        name: "Git",
+        icon: git,
+        category: "Mes outils",
+    },
+    {
+        name: "Github",
+        icon: github,
+        category: "Mes outils",
+    },
+    {
+        name: "Trello",
+        icon: trello,
+        category: "Mes outils",
+    },
+    {
+        name: "Postman",
+        icon: postman,
+        category: "Mes outils",
+    },
+    {
+        name: "Vercel",
+        icon: vercel,
+        category: "Service",
+    },
+];
+
+export default skillsData;
--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,14 +1,35 @@
+import React from 'react';
+import skillsData from "../../assets/data/skills";
+
 function Skills() {
+    // Regrouper les compétences par catégorie
+    const skillsByCategory = skillsData.reduce((acc, skill) => {
+        const { category } = skill;
+        if (!acc[category]) acc[category] = [];
+        acc[category].push(skill);
+        return acc;
+    }, {});
+
     return (
         <section id="compétences" className="skills">
             <div className="container">
-                <h2> MES COMPETENCES</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem nulla pharetra diam. Tristique nulla aliquet enim tortor at. Etiam tempor orci eu lobortis elementum. Aliquam nulla facilisi cras fermentum. Dolor sed viverra ipsum nunc aliquet bibendum enim. Erat nam at lectus urna. Potenti nullam ac tortor vitae purus faucibus ornare. Porttitor rhoncus dolor purus non enim praesent. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Non quam lacus suspendisse faucibus interdum posuere. Quisque id diam vel quam elementum pulvinar. Porttitor leo a diam sollicitudin tempor id eu nisl nunc.
-                </p>
+                <h2>MES COMPETENCES</h2>
+                {Object.entries(skillsByCategory).map(([category, skills]) => (
+                    <div key={category} className="skills-category">
+                        <h3>{category}</h3>
+                        <ul className="skills-list">
+                            {skills.map((skill, index) => (
+                                <li key={index} className="skill-item">
+                                    <img src={skill.icon} alt={`${skill.name} icon`} className="skill-icon" />
+                                    <span>{skill.name}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
             </div>
         </section>
-    )
+    );
 }
 
 export default Skills;
