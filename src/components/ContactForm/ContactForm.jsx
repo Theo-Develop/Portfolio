647fd2d727ff5c5d2934ecbad62819b01bf9a084
Théo Dressy <t.dressy@gmail.com>
2024-03-25 13:58:11 +0100

    settings contact sections

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -44,15 +44,17 @@ function ContactForm() {
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" required />
-            <label>Email</label>
-            <input type="email" name="user_email" required />
-            <label>Message</label>
-            <textarea name="message" required />
-            <input type="submit" value="Send" disabled={isSubmitting} />
-            {stateMessage && <p>{stateMessage}</p>}
+        <form className="form" ref={form} onSubmit={sendEmail}>
+            <label></label>
+            <input type="text" name="user_name" placeholder="Nom" required />
+            <label></label>
+            <input type="email" name="user_email" placeholder="Email" required />
+            <label></label>
+            <textarea name="message" placeholder="Message" required />
+            <input type="submit" className="button" value="Envoyer" disabled={isSubmitting} />
+            <div className="infoForm">
+                {stateMessage && <p>{stateMessage}</p>}
+            </div>
         </form>
     );
 }
