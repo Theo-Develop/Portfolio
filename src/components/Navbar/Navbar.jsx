94ee6f07eedf942f88dc42bf1c45d08af4ae3e08
Théo Dressy <t.dressy@gmail.com>
2024-03-26 14:43:16 +0100

    settings responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 import logo from "../../assets/icons/logo/icon-cygne.webp";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDoorOpen, faUser, faBriefcase, faGraduationCap, faAddressBook } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
     const [activeSection, setActiveSection] = useState("");
@@ -51,27 +53,32 @@ function Navbar() {
             <ul>
                 <li className={isActive("#acceuil") ? "active" : ""}>
                     <Link to="#acceuil" className="Link">
-                        Acceuil
+                        <FontAwesomeIcon icon={faDoorOpen} />
+                        <p className="text">Acceuil</p>
                     </Link>
                 </li>
                 <li className={isActive("#présentation") ? "active" : ""}>
                     <Link to="#présentation" className="Link">
-                        Présentation
+                        <FontAwesomeIcon icon={faUser} />
+                        <p className="text">Présentation</p>
                     </Link>
                 </li>
                 <li className={isActive("#réalisation") ? "active" : ""}>
                     <Link to="#réalisation" className="Link">
-                        Réalisations
+                        <FontAwesomeIcon icon={faBriefcase} />
+                        <p className="text">Réalisations</p>
                     </Link>
                 </li>
                 <li className={isActive("#compétences") ? "active" : ""}>
                     <Link to="#compétences" className="Link">
-                        Compétences
+                        <FontAwesomeIcon icon={faGraduationCap} />
+                        <p className="text">Compétences</p>
                     </Link>
                 </li>
                 <li className={isActive("#contact") ? "active" : ""}>
                     <Link to="#contact" className="Link">
-                        Contact
+                        <FontAwesomeIcon icon={faAddressBook} />
+                        <p className="text"> Contact</p>
                     </Link>
                 </li>
             </ul>
