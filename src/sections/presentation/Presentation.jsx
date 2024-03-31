14a9b941f5efe6069933e29ebdf22dfbed270fe9
Théo Dressy <t.dressy@gmail.com>
2024-03-31 09:41:55 +0200

    settings presentation and mobile responsive

--- src/sections/presentation/Presentation.jsx
+++ src/sections/presentation/Presentation.jsx
@@ -4,7 +4,7 @@ function Presentation() {
             <div className="container">
                 <h2>PRESENTATION</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi scelerisque eu ultrices vitae auctor. Erat velit scelerisque in dictum non consectetur a. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Id ornare arcu odio ut sem nulla pharetra diam sit. Eget magna fermentum iaculis eu non diam phasellus. Nec dui nunc mattis enim ut tellus elementum. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Bibendum neque egestas congue quisque egestas diam in arcu cursus. Maecenas ultricies mi eget mauris pharetra et. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. A cras semper auctor neque vitae tempus quam. Orci dapibus ultrices in iaculis nunc sed. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Sit amet luctus venenatis lectus magna fringilla. Dolor morbi non arcu risus quis varius. Eu feugiat pretium nibh ipsum consequat.
+                Salut! Moi c'est Théo, un développeur web passionné par la création de sites fonctionnels et esthétiquement plaisants. Avec moins d'1 années d'expérience, j'ai travaillé sur une variété de projets que je me propose de vous présenter.
                 </p>
             </div>
         </section>
